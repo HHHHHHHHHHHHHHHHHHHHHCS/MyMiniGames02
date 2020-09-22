51fe7144c11bf7da1c9e1ca92f1842743d3e7fe5
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-09-22 11:34:44 +0800

    add

---
+++ Assets/GodofWarAxeThrow/Scripts/BreakBoxScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace GodofWarAxeThrow.Scripts
+{
+	public class BreakBoxScript : MonoBehaviour
+	{
+		public GameObject breakedBox;
+
+		public void Break()
+		{
+			GameObject breaked = Instantiate(breakedBox, transform.position, transform.rotation);
+			Rigidbody[] rbs = breaked.GetComponentsInChildren<Rigidbody>();
+			foreach (var rb in rbs)
+			{
+				rb.AddExplosionForce(150, transform.position, 30);
+			}
+
+			Destroy(gameObject);
+		}
+	}
+}
