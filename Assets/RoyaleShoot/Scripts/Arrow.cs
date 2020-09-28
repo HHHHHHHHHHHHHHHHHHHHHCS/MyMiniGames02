5bf3d32ed61dc8bbb1c779756bd7bca4ec75c514
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-09-28 11:38:15 +0800

    add

---
+++ Assets/RoyaleShoot/Scripts/Arrow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace RoyaleShoot
+{
+	public class Arrow : MonoBehaviour
+	{
+		public float forceIntensity = 50f;
+
+		private Rigidbody rigi;
+
+		public void Awake()
+		{
+			//arrow 的轴 不对
+			rigi = transform.GetComponent<Rigidbody>();
+			rigi.AddForce(-transform.up * forceIntensity,
+				ForceMode.Impulse);
+			GameObject.Destroy(gameObject, 7f);
+		}
+
+		private void OnCollisionEnter(Collision other)
+		{
+			if (!other.transform.CompareTag("Player"))
+			{
+				rigi.Sleep();
+				rigi.isKinematic = false;
+			}
+		}
+	}
+}
--- Assets/RoyaleShoot/Scripts/PlayerCtrl.cs
+++ Assets/RoyaleShoot/Scripts/PlayerCtrl.cs
@@ -8,13 +8,18 @@ namespace RoyaleShoot
 	public class PlayerCtrl : MonoBehaviour
 	{
 		private static readonly int IsMovingID = Animator.StringToHash("IsMoving");
+		private static readonly int IsZoomID = Animator.StringToHash("IsZoom");
+		private static readonly int IsAttackID = Animator.StringToHash("IsAttack");
+
+		public GameObject arrowPrefab;
 
 		public float velocity = 6f;
 
 		public CinemachineFreeLook cvCam;
-		public bool blockRotationPlayer;
 		public float desiredRotationSpeed = 0.1f;
 		public float allowPlayerRotation = 0.1f;
+		public float arrowTime = 0.6f;
+
 
 		public float zoomFOV;
 		public Vector3 zoomOffset;
@@ -28,10 +33,15 @@ namespace RoyaleShoot
 		private float speed;
 		private bool isMoving;
 		private bool isZooming;
+		private bool blockRotationPlayer;
+
 
 		private CinemachineComposer[] composers;
+		private CinemachineImpulseSource impulse;
 		private float normalFOV;
 		private Vector3 normalOffset;
+		private Tweener zoomTweener;
+		private float arrowTimer;
 
 		private void Start()
 		{
@@ -48,6 +58,8 @@ namespace RoyaleShoot
 			{
 				composers[i] = cvCam.GetRig(i).GetCinemachineComponent<CinemachineComposer>();
 			}
+
+			impulse = cvCam.GetComponent<CinemachineImpulseSource>();
 
 			normalOffset = composers[0].m_TrackedObjectOffset;
 		}
@@ -71,6 +83,11 @@ namespace RoyaleShoot
 				anim.SetBool(IsMovingID, false);
 			}
 
+			if (arrowTimer >= 0)
+			{
+				arrowTimer -= Time.deltaTime;
+			}
+
 			bool rightDown = Input.GetMouseButtonDown(1);
 			bool rightUp = Input.GetMouseButtonUp(1);
 			if (isZooming)
@@ -78,10 +95,19 @@ namespace RoyaleShoot
 				transform.rotation = Quaternion.Slerp(transform.rotation,
 					Quaternion.LookRotation(cam.transform.forward),
 					desiredRotationSpeed);
+
+				if (Input.GetMouseButtonUp(0) && arrowTimer < 0)
+				{
+					anim.SetTrigger(IsAttackID);
+					arrowTimer = arrowTime;
+					OnShoot();
+				}
 			}
 
 			if (rightDown || rightUp)
 			{
+				arrowTimer = arrowTime;
+				anim.SetBool(IsZoomID, rightDown);
 				Zoom(rightDown);
 			}
 		}
@@ -98,39 +124,48 @@ namespace RoyaleShoot
 			right.Normalize();
 
 			var desiredMoveDirection = forward * inputZ + right * inputX;
-
+			blockRotationPlayer = isZooming;
+			
 			if (!blockRotationPlayer)
 			{
 				transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection),
 					desiredRotationSpeed);
-				controller.Move(desiredMoveDirection * (scale * Time.deltaTime * velocity));
 			}
+			controller.Move(desiredMoveDirection * (scale * Time.deltaTime * velocity));
+
 		}
 
 		private void Zoom(bool state)
 		{
-			float fov = state ? zoomFOV : normalFOV;
 			float from = state ? 0 : 1;
 			float to = state ? 1 : 0;
 
+			zoomTweener?.Kill();
 
-			DOVirtual.Float(cvCam.m_Lens.FieldOfView, fov, .1f, SetFieldOfView);
-			DOVirtual.Float(from, to, .2f, SetCameraOffset).SetUpdate(true);
-		}
-
-		private void SetFieldOfView(float fov)
-		{
-			cvCam.m_Lens.FieldOfView = fov;
+			zoomTweener = DOVirtual.Float(from, to, .2f, SetCameraOffset).SetUpdate(true);
 		}
 
 		private void SetCameraOffset(float time)
 		{
 			Vector3 offset = isZooming ? zoomOffset : normalOffset;
 			var xy = Vector2.Lerp(composers[0].m_TrackedObjectOffset, offset, time);
+			float fov = isZooming ? zoomFOV : normalFOV;
+
+			cvCam.m_Lens.FieldOfView = Mathf.Lerp(cvCam.m_Lens.FieldOfView, fov, time);
+
 			foreach (var c in composers)
 			{
 				c.m_TrackedObjectOffset.Set(xy.x, xy.y, c.m_TrackedObjectOffset.z);
 			}
+		}
+
+		private void OnShoot()
+		{
+			impulse.GenerateImpulse();
+			//先放到父物体预设的位置
+			var arrow = Instantiate(arrowPrefab, transform.GetChild(0));
+			//然后脱离父物体
+			arrow.transform.SetParent(null);
 		}
 	}
 }
