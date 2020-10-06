6f2b0b78715dbb6da1889166c2cfacf44221b091
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-10-06 14:20:52 +0800

    add

---
+++ Assets/RoyaleBattle/Scripts/Timeline/CardMarker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace RoyaleBattle.Scripts.Timeline
+{
+	[Serializable, DisplayName("Card Marker")]
+	public class CardMarker : Marker, INotification
+	{
+		public CardData card;
+		public Vector3 position;
+		public Placeable.Faction faction;
+
+		public PropertyName id
+		{
+			get => new PropertyName();
+		}
+	}
+}
