0d40ef6edaa8c23b7e0b69e79cb881e94ec17165
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2021-03-10 21:04:46 +0800

    add

---
+++ Assets/OverwatchDragonstrike/Scripts/DragonMovement.cs
@@ -0,0 +1,67 @@
+using System;
+using DG.Tweening;
+using UnityEngine;
+
+namespace OverwatchDragonstrike.Scripts
+{
+	public class DragonMovement : MonoBehaviour
+	{
+		private static readonly int TimeOffset_ID = Shader.PropertyToID("_TimeOffset");
+		private static readonly int SplitValue_ID = Shader.PropertyToID("_SplitValue");
+
+		public float speed = 5f;
+
+		[Space, Header("Shader Settings")] public float initialDissolveValue = -75f;
+		public float finalDissolveValue = 75f;
+		public float dissolveSpeed = 2f;
+		public float firstDragonOffset = 1f;
+
+		[Space] public float destroyTime = 10f;
+
+		private Renderer[] renderers;
+		private float splitValue;
+
+		private void Start()
+		{
+			renderers = GetComponentsInChildren<Renderer>();
+
+			foreach (var item in renderers[0].materials)
+			{
+				item.SetFloat(TimeOffset_ID, firstDragonOffset);
+			}
+
+			splitValue = initialDissolveValue;
+
+			foreach (var item in renderers)
+			{
+				var materials = item.materials;
+
+				foreach (var mat in materials)
+				{
+					mat.SetFloat(SplitValue_ID, splitValue);
+					// mat.DOFloat(initialDissolveValue, SplitValue_ID, 1).SetDelay(destroyTime)
+					// 	.OnComplete(() => Destroy(gameObject));
+					Destroy(gameObject, destroyTime);
+				}
+			}
+		}
+
+		private void Update()
+		{
+			transform.localPosition += transform.forward * Time.deltaTime * speed;
+
+			foreach (var item in renderers)
+			{
+				var materials = item.materials;
+
+				foreach (var mat in materials)
+				{
+					splitValue += dissolveSpeed * Time.deltaTime * speed;
+
+					//其实可以传入一个起始时间  然后Shader中 _Time.x - startTime
+					mat.SetFloat(SplitValue_ID, splitValue);
+				}
+			}
+		}
+	}
+}
---
+++ Assets/OverwatchDragonstrike/Scripts/DragonStrike.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace OverwatchDragonstrike.Scripts
+{
+	public class DragonStrike : MonoBehaviour
+	{
+		private static readonly int GlowPower_ID = Shader.PropertyToID("_GlowPower");
+		private static readonly int Amount_ID = Shader.PropertyToID("_Amount");
+
+
+		public bool canUltimate = true;
+
+		public Transform arrow;
+
+		public float arrowForce = 3;
+		public float arrowPullDistance = 0.2f;
+		public float arrowPullDuration = 1.0f;
+
+		public float dragonSummonWait = 1f;
+		public GameObject dragonStrikePrefab;
+		public GameObject portalPrefab;
+
+		private Rigidbody arrowRigi;
+
+		private Vector3 arrowLocalPos;
+		private Quaternion arrowLocalRot;
+
+		private WaitForSeconds pullWait;
+		private WaitForSeconds dragWait;
+		private WaitForSeconds ultimateWait;
+
+
+		private void Awake()
+		{
+			arrowRigi = arrow.GetComponent<Rigidbody>();
+			arrowLocalPos = arrow.localPosition;
+			arrowLocalRot = arrow.localRotation;
+
+			pullWait = new WaitForSeconds(arrowPullDuration);
+			dragWait = new WaitForSeconds(dragonSummonWait);
+			ultimateWait = new WaitForSeconds(8f);
+		}
+
+		private void Update()
+		{
+			if (Input.GetMouseButtonDown(0) && canUltimate)
+			{
+				StartCoroutine(UltimateCourotine());
+			}
+
+			if (Input.GetKeyDown(KeyCode.R))
+			{
+				SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+			}
+		}
+
+		private IEnumerator UltimateCourotine()
+		{
+			canUltimate = false;
+
+			//pull arrow
+			arrow.DOLocalMoveZ(arrow.localPosition.z - arrowPullDistance, arrowPullDuration).SetEase(Ease.OutExpo);
+			var arrowRender = arrow.GetComponent<Renderer>();
+			arrowRender.material.DOFloat(1, GlowPower_ID, arrowPullDuration);
+
+			var mainCam = Camera.main;
+
+			var oldFov = mainCam.fieldOfView;
+
+			mainCam.DOFieldOfView(65, arrowPullDuration).SetEase(Ease.OutSine);
+
+			yield return pullWait;
+
+			ThrowArrow();
+
+			mainCam.DOFieldOfView(oldFov, 0.5f).SetEase(Ease.OutBack);
+			var q = Quaternion.LookRotation(transform.forward, Vector3.up);
+
+			yield return dragWait;
+
+			//Instantiate
+			var dragon = Instantiate(dragonStrikePrefab, arrow.position, q);
+			var portal = Instantiate(portalPrefab, arrow.position - (arrow.forward * 5) + (Vector3.up * 1.2f), q);
+
+			//Show Portal
+			portal.transform.DOScale(0, 0.2f).SetEase(Ease.OutSine).From();
+			portal.GetComponent<Renderer>().material.DOFloat(1, Amount_ID, 4f).SetDelay(8)
+				.OnComplete(() => Destroy(portal));
+
+			ParticleSystem[] portalParticles = portal.GetComponentsInChildren<ParticleSystem>();
+			foreach (var item in portalParticles)
+			{
+				item.Play();
+			}
+
+			//Extras
+			arrow.GetComponent<TrailRenderer>().emitting = false; //拖尾轨迹
+			arrowRender.enabled = false;
+			arrow.parent = transform.GetChild(0);
+			arrow.transform.localPosition = arrowLocalPos;
+			arrow.transform.localRotation = arrowLocalRot;
+			arrowRender.enabled = true;
+			arrowRender.material.SetFloat(GlowPower_ID, 1f);
+			arrowRender.material.DOFloat(0.0f, GlowPower_ID, 0.5f);
+
+			mainCam.transform.DOShakePosition(0.2f, 0.5f, 20, 90, false, true);
+
+			yield return ultimateWait;
+			canUltimate = true;
+		}
+
+		private void ThrowArrow()
+		{
+			arrow.parent = null;
+			arrowRigi.isKinematic = false;
+			arrowRigi.AddForce(-arrow.forward * arrowForce, ForceMode.Impulse);
+			arrow.GetComponent<TrailRenderer>().enabled = true;
+
+			arrow.GetComponent<Renderer>().material.SetFloat(GlowPower_ID, 0f);
+
+			ParticleSystem[] portalParticles = arrow.GetComponentsInChildren<ParticleSystem>();
+			foreach (var item in portalParticles)
+			{
+				item.Play();
+			}
+		}
+	}
+}
