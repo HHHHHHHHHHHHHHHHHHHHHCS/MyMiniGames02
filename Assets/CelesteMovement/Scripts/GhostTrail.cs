04bfe459695b5314ee8861dc9bf9918468ff9b03
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2021-07-20 20:46:01 +0800

    add

---
+++ Assets/CelesteMovement/Scripts/GhostTrail.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace CelesteMovement.Scripts
+{
+    public class GhostTrail : MonoBehaviour
+    {
+
+    }
+}
---
+++ Assets/CelesteMovement/Scripts/PlayerAnimation.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace CelesteMovement.Scripts
+{
+    public class PlayerAnimation : MonoBehaviour
+    {
+    
+    }
+}
