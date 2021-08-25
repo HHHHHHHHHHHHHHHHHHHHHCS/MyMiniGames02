d3359577e8935b8e7d30bb4ead5ec1693d8e7c8f
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2021-08-25 20:48:29 +0800

    add

--- Assets/MarioKartDrift/Scripts/KartController.cs
+++ Assets/MarioKartDrift/Scripts/KartController.cs
@@ -70,30 +70,36 @@ namespace MarioKartDrift.Scripts
 				Time.timeScale = time;
 			}
 
-			
-			
+			float hor = Input.GetAxis("Horizontal");
+			int horRaw = (int) Input.GetAxisRaw("Horizontal");
+			// float ver = Input.GetAxis("Vertical");
+			// int verRaw = (int) Input.GetAxisRaw("Vertical");
+			bool isJumpDown = Input.GetButtonDown("Jump");
+			bool isJumpUp = Input.GetButtonUp("Jump");
+			bool isAcc = Input.GetButton("Fire1");
+
 			//Follow Collider 
 			transform.position = sphere.transform.position - new Vector3(0f, 0.4f, 0f);
 
 			//Accelerate
-			if (Input.GetButton("Fire1"))
+			if (isAcc)
 			{
 				speed = acceleration;
 			}
 
 			//Steer
-			if (Input.GetAxis("Horizontal") != 0)
+			if (hor != 0)
 			{
-				int dir = (int) Input.GetAxisRaw("Horizontal");
-				float amount = Mathf.Abs(Input.GetAxis("Horizontal"));
+				int dir = horRaw;
+				float amount = Mathf.Abs(hor);
 				Steer(dir, amount);
 			}
 
 			//Drift
-			if (Input.GetButtonDown("Jump") && !drifting && Input.GetAxis("Horizontal") != 0)
+			if (isJumpDown && !drifting && hor != 0)
 			{
 				drifting = true;
-				driftDirection = (int) Input.GetAxisRaw("Horizontal");
+				driftDirection = horRaw;
 
 				foreach (ParticleSystem p in primaryParticles)
 				{
@@ -120,7 +126,7 @@ namespace MarioKartDrift.Scripts
 					to2 = 0;
 				}
 
-				float control = math.remap(Input.GetAxis("Horizontal"), -1, 1, from2, to2);
+				float control = math.remap(hor, -1, 1, from2, to2);
 
 				if (driftDirection == 1)
 				{
@@ -133,13 +139,13 @@ namespace MarioKartDrift.Scripts
 					to2 = 0.2f;
 				}
 
-				float powerControl = math.remap(Input.GetAxis("Horizontal"), -1, 1, from2, to2);
+				float powerControl = math.remap(hor, -1, 1, from2, to2);
 				Steer(driftDirection, control);
 				driftPower += powerControl;
 				ColorDrift();
 			}
 
-			if (Input.GetButtonUp("Jump") && drifting)
+			if (isJumpUp && drifting)
 			{
 				Boost();
 			}
@@ -148,14 +154,70 @@ namespace MarioKartDrift.Scripts
 			speed = 0f;
 			currentRotate = Mathf.Lerp(currentRotate, rotate, Time.deltaTime * 4f);
 			rotate = 0f;
-			
+
 			//Animations    
 
 			//a) Kart
 			if (!drifting)
 			{
-				
+				kartModel.localEulerAngles = Vector3.Lerp(kartModel.localEulerAngles,
+					new Vector3(0, 90 + (hor * 15), kartModel.localEulerAngles.z), 0.2f);
 			}
+			else
+			{
+				float from2, to2;
+				if (driftDirection == 1)
+				{
+					from2 = 0.5f;
+					to2 = 2f;
+				}
+				else
+				{
+					from2 = 2f;
+					to2 = 0.5f;
+				}
+
+				float control = math.remap(hor, -1, 1, from2, to2);
+				kartModel.parent.localRotation = quaternion.Euler(0,
+					Mathf.LerpAngle(kartModel.parent.localEulerAngles.y, (control * 15) * driftDirection, 0.2f), 0f);
+			}
+
+			//b) Wheels
+			frontWheels.localEulerAngles = new Vector3(0, hor * 15f, frontWheels.localEulerAngles.z);
+			frontWheels.localEulerAngles += new Vector3(0f, 0f, sphere.velocity.magnitude / 2f);
+			backWheels.localEulerAngles += new Vector3(0f, 0f, sphere.velocity.magnitude / 2f);
+
+			//c) Steering Wheel
+			steeringWheel.localEulerAngles = new Vector3(-25f, 90, hor * 45);
+		}
+
+		private void FixedUpdate()
+		{
+			//Forward Acceleration
+			if (!drifting)
+			{
+				sphere.AddForce(-kartModel.transform.right * currentSpeed, ForceMode.Acceleration);
+			}
+			else
+			{
+				sphere.AddForce(transform.forward * currentSpeed, ForceMode.Acceleration);
+			}
+
+			//Gravity
+			sphere.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
+
+			//Steering
+			transform.eulerAngles = Vector3.Lerp(transform.eulerAngles,
+				new Vector3(0, transform.eulerAngles.y + currentRotate, 0), Time.deltaTime * 5f);
+
+			RaycastHit hitOn, hitNear;
+
+			Physics.Raycast(transform.position + (transform.up * 0.1f), Vector3.down, out hitOn, 1.1f, layerMask);
+			Physics.Raycast(transform.position + (transform.up * 0.1f), Vector3.down, out hitNear, 2.0f, layerMask);
+
+			//Normal Rotation
+			kartNormal.up = Vector3.Lerp(kartNormal.up, hitNear.normal, Time.deltaTime * 8.0f);
+			kartNormal.Rotate(0, transform.eulerAngles.y, 0);
 		}
 	}
 }
